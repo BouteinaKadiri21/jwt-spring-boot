ac46aff654a139cc5d5b6d6f17c989b9e84923ab
Bouteina Kadiri <49611471+BouteinaKadiri21@users.noreply.github.com>
2021-06-24 06:28:12 -0700

    one by one !!

---
+++ AmigoSecurityApplication.java
@@ -0,0 +1,13 @@
+package ca.bkyh21.amigoSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoSecurityApplication.class, args);
+	}
+
+}
